fe316c2a8a674dce4257a5f9f3ca168cc8e4bbe2
Ashing Zheng <axingfly@gmail.com>
2026-05-29 08:48:51 +0800

    chore: add Google AdSense setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
+          id="google-adsense"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3696025303089929"
+          crossOrigin="anonymous"
+        />
+        <Script
           defer
           data-domain="playbackstats.com"
           src="https://plausible.vibecodinghub.org/js/script.file-downloads.outbound-links.js"
